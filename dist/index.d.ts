288b75d050fc2af8a686d3a9158db1f110be7418
SlamDevelop <slam4ua@gmail.com>
2022-06-29 22:40:21 +0300

    Build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,7 @@ import Button from "./components/Button";
 import Input from "./components/Input";
 import * as Modal from "./components/Modal";
 import * as Tabs from "./components/Tabs";
+import type { ButtonType, ButtonColor } from "./components/Button";
+import type { InputType, InputColor } from "./components/Input";
 export { Button, Input, Modal, Tabs };
+export type { ButtonType, ButtonColor, InputType, InputColor };
