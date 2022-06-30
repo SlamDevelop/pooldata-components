33f60bf04105b4f57d414176ef7a98f6e38a1b4c
SlamDevelop <slam4ua@gmail.com>
2022-06-30 16:57:21 +0300

    FIx: Corrected global styles stitches

--- src/components/Input/Input.style.tsx
+++ src/components/Input/Input.style.tsx
@@ -12,15 +12,19 @@ const Input = styled('input', {
     variants: {
         color: {
             blue: {
+                '&::placeholder': { color: '$colors$blue' },
+
                 backgroundRGBA: '$rgb$blue',
                 backgroundOpacity: 0.1,
                 color: '$colors$blue',
                 '&:focus': { boxShadow: '0 0 0 1px $colors$blue' },
             },
             yellow: {
+                '&::placeholder': { color: '$colors$yellowDark' },
+
                 backgroundRGBA: '$rgb$yellow',
-                backgroundOpacity: 0.1,
-                color: '$colors$yellow',
+                backgroundOpacity: 0.15,
+                color: '$colors$yellowDark',
                 '&:focus': { boxShadow: '0 0 0 1px $colors$yellow' },
             },
         },
